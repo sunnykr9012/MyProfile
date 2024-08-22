222fb6592f8f68b90529c11e436e64faca260908
SUNNY <113082105+sunnykr9012@users.noreply.github.com>
2024-08-22 21:55:36 +0530

    first and push

---
+++ data/index.ts
@@ -0,0 +1,212 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Data Transforming time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve AWS, ML, AI, Python ",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for Data Analyst.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Code Data Transforming",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Full WordPress Website,",
+    des: "Develop Offline This Site Localhost Xampp Server And Wordpress, Now Live Hostinger ",
+    img: "/p1.png",
+    iconLists: [
+      "/icons8-wordpress.svg",
+      "/xampp.svg",
+      "/FileZilla_logo.svg",
+      "/host.svg",
+    ],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "NLP in Tensor Flow",
+    des: "Data Accuracy Check. Using Helper Funcation, Tensorflow, Pipline, etc",
+    img: "/nlpa.png",
+    iconLists: [
+      "/nlp.png",
+      "/google-tensorflow-icon.svg",
+      "/Google_Colaboratory_SVG_Logo.svg",
+    ],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "PowerBI Intractive Dashboard Real Time Data Dashboard",
+    des: "A Real Time Data Dashboard PowerBI Cleaning Data, Transforming Data, etc.",
+    img: "/powerbidashboard.png",
+    iconLists: ["/Power-BI.png"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote: "",
+    name: "Coming Soon....",
+    title: "",
+  },
+  {
+    quote:
+      "",
+    name: "Coming Soon....",
+    title: "",
+  },
+  {
+    quote:
+      ".",
+    name: "Coming Soon....",
+    title: "",
+  },
+  {
+    quote:
+      "",
+    name: "Coming Soon....",
+    title: "",
+  },
+  {
+    quote:
+      "",
+    name: "Coming Soon....",
+    title: "",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    img: "/icons8-aws-100.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+
+    img: "/chip.png",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 3,
+
+    img: "/icons8-python-1000.png",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 4,
+
+    img: "/Power-BI.png",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 5,
+
+    img: "/sql.png",
+    nameImg: "/cloudName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web Developer Intern",
+    desc: "Assisted in the development of a web-based platform using WordPress,HTML,CSS,JS,React.js,enhancing interactivity Web Application .",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Data Anlayst",
+    desc: "Transformaing Data, Creative Dashboard, AWS, Python, Pandas, NumPy, NLP, Tensorflow, PowerBI.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  
+  
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
